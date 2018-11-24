144825174fdfb80826ce28e597a26a2f39025131
Nedyalko Dyakov <me@ndyakov.eu>
2018-11-24 13:36:07 +0200

    fix keybindings.

--- fish/conf.d/config.fish
+++ fish/conf.d/config.fish
@@ -1,8 +1,6 @@
-set fish_plugins autojump vi-mode
-set vi_mode_default vi_mode_normal
-
-function fish_user_key_bindings
-  fish_vi_key_bindings
-end
+# vi bindings
+set -u fish_plugins autojump vi-mode
+set -u vi_mode_default vi_mode_normal
+set -u fish_key_bindings fish_vi_key_bindings
 
 set -gx GOPATH $HOME/go; set -gx GOROOT $HOME/.go; set -gx PATH $GOPATH/bin $PATH; # g-install: do NOT edit, see https://github.com/stefanmaric/g
